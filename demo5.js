7dc33e352bcacf7ee2b8749ea74a7dfa08d73613
ParthGurval <sohamgurval2001@gmail.com>
2023-07-24 13:14:59 +0530

    Function with expression
    assignment done on function expression on 03_functionExprAssign.js

--- demo5.js
+++ demo5.js
@@ -1,12 +1,20 @@
 
 
-var str = "Hello I'm Siri";
-var strLength = str.length;
+// var str = "Hello I'm Siri";
+// var strLength = str.length;
 
-console.log(str, 'Length of given String is:', strLength);
+// console.log(str, 'Length of given String is:', strLength);
 
-var charAtValue = str.charAt(7);
-console.log(`${str} - char at 0th position is: ${charAtValue}`);
+// var charAtValue = str.charAt(7);
+// console.log(`${str} - char at 0th position is: ${charAtValue}`);
 
-var charLast = str.charAt(strLength-1);
-console.log(` Last Character is ${charLast} of index value is: ${strLength}`);
+// var charLast = str.charAt(strLength-1);
+// console.log(` Last Character is ${charLast} of index value is: ${strLength}`);
+
+var num = 100;
+console.log(num);
+var display = function(){
+    console.log(`I am Angular Developer !`);
+}
+display();
+console.log(typeof display);
