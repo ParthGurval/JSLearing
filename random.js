b1443436d0a964cfcb80c1d5f869dbdb85510f5c
ParthGurval <sohamgurval2001@gmail.com>
2023-08-27 12:23:59 +0530

    Attendance of Saturday 26-08-2023 and Sunday 27-08-2023
    Attendance of Saturday 26-08-2023 and Sunday 27-08-2023 and practice of coding

--- random.js
+++ random.js
@@ -1,8 +1,8 @@
-// const randomNumber1 = Math.random();
-// const randomNumber2 = Math.random();
+const randomNumber1 = Math.random();
+const randomNumber2 = Math.random();
 
-// console.log(`Random number 1: ${randomNumber1}`);
-// console.log(`Random number 2: ${randomNumber2}`);
+console.log(`Random number 1: ${randomNumber1}`);
+console.log(`Random number 2: ${randomNumber2}`);
 
 
 // const names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve'];
